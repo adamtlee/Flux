c0075a31d241abc6ec02bc082844454cbafee534
adamtlee <adam.lee346@gmail.com>
2026-02-27 17:13:30 -0700

    Refactored BankAccountController

--- src/Flux.Data/obj/Debug/net10.0/Flux.Data.AssemblyInfo.cs
+++ src/Flux.Data/obj/Debug/net10.0/Flux.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flux.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1333fcc377ca1a26dabb91238cb7e0744b5233bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f531579e278d71ba6d59f2f9610570138beda4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flux.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flux.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
