73e7155025e04f0ed0e39c51a898179af804b0e8
adamtlee <adam.lee346@gmail.com>
2026-03-02 17:20:25 -0700

    Added proxy conf file for angular

--- src/Flux.Services/obj/Debug/net10.0/Flux.Services.AssemblyInfo.cs
+++ src/Flux.Services/obj/Debug/net10.0/Flux.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flux.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c084e3f520480e94bab6eefc07c1a4892d14a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1c39f2c5682b7ae5023e5b24ab8433581a0a56b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flux.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flux.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
